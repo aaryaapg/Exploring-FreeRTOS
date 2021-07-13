e8b89a292eaeff3182ebedee57dd7039bcce1b40
aaryaapg <aaryaapg@gmail.com>
2021-07-13 16:06:40 +0530

    init

---
+++ 01_Simultaneous_Blink/src/main.cpp
@@ -0,0 +1,50 @@
+/*
+* This code 
+*/
+
+/* ______________________________________Libraries______________________________________ */
+#include <Arduino.h>
+/* _______________________________________Macros________________________________________ */
+/* _____________________________Declarations_and_Variables______________________________ */
+int count1 = 0;
+int count2 = 0;
+/* ______________________________________Functions______________________________________ */
+void task1(void * parameters){
+    while(1){ // infinite loop
+        Serial.print("Task 1 counter: ");
+        Serial.println(count1++);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+void task2(void * parameters){
+    while(1){ // infinite loop
+        Serial.print("Task 2 counter: ");
+        Serial.println(count2++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+/* ________________________________________Setup________________________________________ */
+void setup() {
+  Serial.begin(115200);
+  xTaskCreate(
+        task1,    // Function that should be called
+        "Task 1", // Name of the task (for debugging)
+        1000,     // Stack size (bytes)
+        NULL,     // Parameter to pass
+        1,        // Task priority
+        NULL      // Task handle
+  );
+
+	xTaskCreate(
+        task2,    // Function that should be called
+        "Task 2", // Name of the task (for debugging)
+        1000,     // Stack size (bytes)
+        NULL,     // Parameter to pass
+        1,        // Task priority
+        NULL      // Task handle
+  );
+}
+/* ________________________________________Loop_________________________________________ */
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Exploring FreeRTOS on ESP32
+# References
+1. https://savjee.be/videos/programming-esp32-with-arduino/what-is-freertos/

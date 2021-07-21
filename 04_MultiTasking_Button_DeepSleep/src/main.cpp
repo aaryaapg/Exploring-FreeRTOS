d07e4a7511125d59fc39aacd2c6a795cf652ab9d
aaryaapg <aaryaapg@gmail.com>
2021-07-21 15:34:03 +0530

    Added MPU6050

--- 04_MultiTasking_Button_DeepSleep/src/main.cpp
+++ 04_MultiTasking_Button_DeepSleep/src/main.cpp
@@ -15,6 +15,15 @@ Red     -------> 13
 Anode   -------> 3.3V
 Green   -------> 12
 Blue    -------> 14
+MPU6050       ESP32
+VCC --------> 3.3V         
+GND --------> GND 
+SCL --------> 26
+SDA --------> 25
+XDA --------> NC
+XCL --------> NC
+AD0 --------> GND
+INT --------> NC
 
 -> https://github.com/espressif/arduino-esp32/issues/595#issuecomment-413829444
 -> https://gist.github.com/SaheblalBagwan/5e538a727a5f1b0c129da36786d67159
@@ -28,6 +37,9 @@ Blue    -------> 14
 //WD
 #include "soc/timer_group_struct.h"
 #include "soc/timer_group_reg.h"
+//MPU6050
+#include <Wire.h> //For communication between MPU6050 and ESP32 (I2C)
+#include <Math.h>
 
 /* _______________________________________Macros________________________________________ */
 //RFID
@@ -39,6 +51,14 @@ Blue    -------> 14
 #define RED 13
 #define GREEN 12
 #define BLUE 14
+//For MPU6050 - threshod for number of rotations
+#define Upper_Threshold 0.85
+//MPU6050 I2C Pins
+#define SCL 26
+#define SDA 25
+//Define sensitivity scale factors for accelerometer and gyro based on the GYRO_CONFIG and ACCEL_CONFIG register values
+#define aScaleFactor 16384
+#define gScaleFactor 131
 /* _____________________________Declarations_and_Variables______________________________ */
 //RFID
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
@@ -50,9 +70,31 @@ int buttonState; //Logout will be initiated if button state is HIGH
 //Tasks
 TaskHandle_t taskRFID_handle = NULL;
 TaskHandle_t taskReadSensors_handle = NULL;
-int count = 0;
 TaskHandle_t taskLogoutButton_handle = NULL;
 TaskHandle_t taskLogoutProcedure_handle = NULL;
+//MPU6050: Since pin AD0 is Grounded, address of the device is b1101000 (0x68) [Check Sec 9.2 of Datasheet]
+const uint8_t MPU6050SlaveAddress = 0x68;
+//MPU6050: Few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B; //Register 59 (14 Registers (59 to 72) contain accel, temp and gyro data)
+//Accelerometer and Gyroscope Variabels
+int16_t AX_raw, AY_raw,AZ_raw, GX_raw, GY_raw, GZ_raw, Temp_raw;
+double Ax, Ay, Az, T, Gx, Gy, Gz;
+//Counter Variables for Accelerometer Rotations
+int state; //State Variable
+int c_state = 0; //current state
+int p_state = 0; //previous state
+int count = 0; //counting all edges
+int realcount; //Count for just low to high edges
 
 /* ______________________________________Functions______________________________________ */
 //Function to get RGB colours
@@ -61,6 +103,51 @@ void RGB_color(bool r, bool g, bool b){
   digitalWrite(GREEN, g);
   digitalWrite(BLUE, b);
   delay(1000);
+}
+//A function that lets us write data to the slave's registers easily
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data) {
+  Wire.beginTransmission(deviceAddress); //Get the slave's attention, tell it we're sending a command byte. Slave = MPU6050
+  Wire.write(regAddress); //The command byte sets pointer to register whose address is given
+  Wire.write(data); //Now that the pointer is ‘pointing’ at the specific register you wanted, this command will replace the byte stored in that register with the given data.
+  Wire.endTransmission(); //This tells the slave that you’re done giving it instructions for now
+}
+//Configure and setup MPU6050 Registers
+void MPU6050_Init() {
+  delay(150);
+  //Step 1: Set sample rate divider to get the desired sampling rate of 1kHz based on the formula given in datasheet
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  //Step 2: Set PLL with X axis gyroscope as the clock reference for improved stability.
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  //Step 3: This functionality is not required. Disable it.
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  //Step 4: Disable external Frame Synchronization and disable DLPF so that Gyroscope Output Rate = 8kHz
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  //Step 5: Set gyroscope full range to +- 250 dps, so that the gyroscope sensitivity scale factor is 131
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);
+  //Step 6: Set accelerometer full range to +- 2g, so that the accelerometer sensitivity scale factor is 16384
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);
+  //Step 7: This functionality is not required. Disable it.
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  //Step 8: Enable the Data Ready interrupt
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  //Step 9: Do not reset signal paths
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  //Step 10: The functionalities provided by the bits of this register are not requires now. Disable them
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
+}
+//14 Registers (59 to 72) contain accel, temp and gyro data. We need to access it
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress) {
+  Wire.beginTransmission(deviceAddress); //Get the slave's attention, tell it we're sending a command byte. Slave = MPU6050
+  Wire.write(regAddress); //The command byte sets pointer to register whose address is given
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14); //Used by the master to request bytes from a slave device
+  AX_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  AY_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  AZ_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  Temp_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  GX_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  GY_raw = (((int16_t)Wire.read() << 8) | Wire.read());
+  GZ_raw = (((int16_t)Wire.read() << 8) | Wire.read());
 }
 /* ________________________________________Tasks________________________________________ */
 void taskLogoutProcedure(void * parameter){
@@ -100,12 +187,40 @@ void taskReadSensors(void * parameter){
   delay(1000);
   Serial.println("Reading Sensors");
   for(;;){
-    Serial.print("Sensor counter: ");
-    Serial.println(count++);
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    //Read sensors here
+    Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+    //Divide each with their sensitivity scale factor
+    Ax = (double)AX_raw / aScaleFactor;
+    Ay = (double)AY_raw / aScaleFactor;
+    Az = (double)AZ_raw / aScaleFactor;
+    T = (double)Temp_raw / 340 + 36.53; //temperature formula from datasheet
+    Gx = (double)GX_raw / gScaleFactor;
+    Gy = (double)GY_raw / gScaleFactor;
+    Gz = (double)GZ_raw / gScaleFactor; 
+    //Comparator
+    if(Az > Upper_Threshold) {
+      state = 1;
+    }
+    else {
+      state = 0;
+    }
+    c_state = state;
+    // compare the current state to its previous state
+    if (c_state != p_state) {
+      // if the state has changed, increment the counter
+      count++;
+      // Delay a little bit to avoid bouncing
+    }
+    else{
+      count = count;
+    }
+    // save the current state as the last state, for next time through the loop
+    p_state = c_state;
+    realcount = count / 2;
+    Serial.print(" State: "); Serial.print(state);
+    Serial.print(" Count: "); Serial.println(realcount);
+    delay(500);
   }
-  //Serial.println("Ending task 2");
-  //vTaskDelete( NULL );
 }
 void taskRFID( void * parameter ){
   for(;;){
@@ -166,6 +281,9 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
+  //Setup MPU6050
+  Wire.begin(SDA, SCL);
+  MPU6050_Init(); //Setup the MPU6050 registers
   //RGB LED
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
